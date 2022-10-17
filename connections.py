b2f20c5d8e96538795084a46ab3b49fc667139b4
Manuel Ledezma <92046228+manuelledezma687@users.noreply.github.com>
2022-10-17 16:30:13 -0300

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,11 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:password@0.0.0.0:5000/fivestars"
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = "mysql+mysqlconnector://root:password@0.0.0.0:5000/fivestars"
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+# engine = create_engine(DATABASE_URL)
 
-Base = declarative_base()
+# SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+# Base = declarative_base()
+
+from curses import meta
+from sqlalchemy import create_engine, MetaData
+
+engine = create_engine("mysql+mysqlconnector://admin:password@localhost:3306/fivestars"")
+
+meta_data = MetaData()
