d6267777d10bfb5c49d0fafa6e4e6347cd4a8e51
manuelledezma687 <mledezma@teco.com.ar>
2022-10-17 15:11:17 -0300

    testing

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
+from typing import Union
+
 from fastapi import FastAPI
-from paths import home, bookings, users, contact, ratings
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import status
 
 app = FastAPI()
 
-#Paths from the proyect.
-app.include_router(home.router)
-app.include_router(bookings.router)
-app.include_router(ratings.router)
-app.include_router(users.router)
-app.include_router(contact.router)
 
-
-
-origins = [
-    'http://localhost:5173/',
-    'https://fivestars.bibliotecadeltester.org/',
-    'https://fivestarscarservice.com/',
-    '*'    
-]
-    
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins= origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
