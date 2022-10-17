4c6e72b3ca2c81cb3a5cf3fc04ba53aacfa9b6dd
manuelledezma687 <mledezma@teco.com.ar>
2022-10-17 15:51:53 -0300

    admin

--- connections.py
+++ connections.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:password@localhost:3306/fivestars"
+DATABASE_URL = "mysql+mysqlconnector://admin:password@localhost:3306/fivestars"
 
 engine = create_engine(DATABASE_URL)
 
